7e367169f2542214bee3bcef8ef9fcec2a890f4b
YerikZZY <YerikZZY@users.noreply.github.com>
2021-02-27 21:02:47 -0600

    Initial Commit

---
+++ ZeyuZTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZeyuZTableView
+//
+//  Created by Jay Zhang on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
